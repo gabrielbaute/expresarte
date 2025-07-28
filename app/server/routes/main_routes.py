031abe09546d9f1ad156418b18ea32a40317dd18
gabrielbaute <gabrielbaute@gmail.com>
2025-07-28 19:04:29 -0400

    Redirecciona si no está logeado

--- app/server/routes/main_routes.py
+++ app/server/routes/main_routes.py
@@ -5,10 +5,13 @@ from flask import(
     render_template,
     flash,
 )
+from flask_login import login_required, current_user
 
 main_bp = Blueprint('main', __name__, template_folder='templates')
 
 # Rutas de la navegación de la app
 @main_bp.route('/')
 def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
     return render_template('main/index.html')
