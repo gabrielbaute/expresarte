96cc827336c8adb6322b0c93f5da0ffa229b80c7
gabrielbaute <gabrielbaute@gmail.com>
2025-08-02 16:52:01 -0400

    Se vincula con período académico. Se renombra alumno por estudiante

--- app/database/models/calificaciones_model.py
+++ app/database/models/calificaciones_model.py
@@ -9,23 +9,25 @@ class Calificacion(db.Model):
     __tablename__ = 'calificacion'
 
     id = db.Column(db.Integer, primary_key=True)
-    alumno_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
+    estudiante_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
     catedra_academica_id = db.Column(db.Integer, db.ForeignKey("catedra_academica.id"))
+    periodo_id = db.Column(db.Integer, db.ForeignKey("periodo_academico.id"))
     calificacion = db.Column(db.Enum(Calificacion), nullable=True)
     observaciones = db.Column(db.Text)
     fecha = db.Column(db.DateTime)
 
     def __repr__(self) -> str:
-        return f'<Evaluacion alumno_id={self.alumno_id} catedra_academica_id={self.catedra_academica_id}>'
+        return f'<Evaluacion alumno_id={self.estudiante_id} catedra_academica_id={self.catedra_academica_id}>'
     
     # Relaciones
+    periodo = db.relationship("PeriodoAcademico")
     alumno = db.relationship('Usuario', back_populates='calificaciones')
     catedra_academica = db.relationship('CatedraAcademica', back_populates='calificaciones')
     
     def to_dict(self) -> Dict[str, any]:
         return {
             'id': self.id,
-            'alumno_id': self.alumno_id,
+            'alumno_id': self.estudiante_id,
             'catedra_academica_id': self.catedra_academica_id,
             'calificacion': self.calificacion.value if self.calificacion else None,
             'observaciones': self.observaciones,
