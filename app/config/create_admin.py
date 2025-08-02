f7762ea4f9cbaf2ccc03a190960a94187611d0e7
gabrielbaute <gabrielbaute@gmail.com>
2025-08-02 16:49:06 -0400

    Se crea usuario empleando Schemas

--- app/config/create_admin.py
+++ app/config/create_admin.py
@@ -1,8 +1,20 @@
 from typing import Optional
 from flask import current_app
+from werkzeug.security import generate_password_hash
 from app.controllers import ControllerFactory
 from app.config.settings import Config
 from app.database.enums import Role, Sexo
+from app.schemas import UserCreate
+
+def build_user(email: str, password: str, nombre: str, apellido: str, role: Role, sexo: Sexo) -> UserCreate:
+    return UserCreate(
+        email=email,
+        password_hash=password,
+        primer_nombre=nombre,
+        primer_apellido=apellido,
+        role=role,
+        sexo=sexo
+    )
 
 def create_initial_super_admin() -> Optional[bool]:
     """Crea el usuario super_admin al iniciar la aplicación por primera vez."""
@@ -26,15 +38,16 @@ def create_initial_super_admin() -> Optional[bool]:
             return True
 
         # Crear el super admin
-        super_admin = controller.create_user(
+        new_admin = UserCreate(
             primer_nombre=admin_nombre,
             primer_apellido=admin_apellido,
             email=admin_email,
-            password=admin_password,
+            password_hash=generate_password_hash(admin_password),
             role=Role.SUPER_ADMIN,
             sexo=Sexo.NO_APLICA,
             activo=True
         )
+        super_admin = controller.create_user(new_admin)
 
         if not super_admin:
             current_app.logger.warning("No se pudo crear el usuario super admin por motivos desconocidos")
