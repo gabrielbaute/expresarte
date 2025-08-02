875c9541d502bf38641d5dfa1d0e4b738e6bc324
gabrielbaute <gabrielbaute@gmail.com>
2025-08-02 16:52:59 -0400

    Se renombra alumno por estudiante para homologar con los schemas.

--- app/database/models/inscripciones_model.py
+++ app/database/models/inscripciones_model.py
@@ -9,7 +9,7 @@ class Inscripcion(db.Model):
     __tablename__ = 'inscripcion'
 
     id = db.Column(db.Integer, primary_key=True)
-    alumno_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
+    estudiante_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
     catedra_academica_id = db.Column(db.Integer, db.ForeignKey("catedra_academica.id"))
     periodo_id = db.Column(db.Integer, db.ForeignKey("periodo_academico.id"))
     fecha_inscripcion = db.Column(db.DateTime, default=datetime.utcnow)
@@ -21,12 +21,12 @@ class Inscripcion(db.Model):
     periodo = db.relationship("PeriodoAcademico", back_populates="inscripciones")
 
     def __repr__(self) -> str:
-        return f'<Inscripcion alumno_id={self.alumno_id} catedra_academica_id={self.catedra_academica_id}>'
+        return f'<Inscripcion estudiante_id={self.estudiante_id} catedra_academica_id={self.catedra_academica_id}>'
     
     def to_dict(self) -> Dict[str, any]:
         return {
             'id': self.id,
-            'alumno_id': self.alumno_id,
+            'estudiante_id': self.estudiante_id,
             'catedra_academica_id': self.catedra_academica_id,
             'fecha_inscripcion': self.fecha_inscripcion,
             'estado': self.estado
