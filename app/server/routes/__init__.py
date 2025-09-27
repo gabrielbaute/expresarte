25b55b7d618cf49a6f30ca79e10d58d49ff6761b
gabrielbaute <gabrielbaute@gmail.com>
2025-09-27 17:51:46 -0400

    Docstrings y tipado explicito para el registro de blueprints

--- app/server/routes/__init__.py
+++ app/server/routes/__init__.py
@@ -1,10 +1,16 @@
 """Registro de los blueprints"""
+from flask import Flask
 from app.server.routes.main_routes import main_bp
 from app.server.routes.auth_routes import auth_bp
 from app.server.routes.admin_routes import admin_bp
 from app.server.routes.teacher_routes import teacher_bp
 
-def register_blueprints(app):
+def register_blueprints(app: Flask) -> None:
+    """Registro de los blueprints
+    
+    Args:
+        app (Flask): instancia de la app Flask
+    """
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
