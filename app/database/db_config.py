2b7a89b2a64b03375e72d11a7ed85ed4070f62f4
gabrielbaute <gabrielbaute@gmail.com>
2025-09-27 09:52:49 -0400

    Docstrings para documentación. Tipado para init_db: se especifica que debe recibir una app tipo flask

--- app/database/db_config.py
+++ app/database/db_config.py
@@ -1,6 +1,12 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def init_db(app):
+def init_db(app: Flask):
+    """Inicializa la base de datos
+    
+    Args:
+        app (Flask): Instancia de la aplicación Flask
+    """
     db.init_app(app)
